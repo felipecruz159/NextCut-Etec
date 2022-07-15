ddbc168928a2cfa5f694eb813cf549f0ea896ed7
Rafa1922 <faelgomes2003@gmail.com>
2022-07-14 21:15:34 -0300

    Tela-inicio

--- cadastro_escolha.php
+++ cadastro_escolha.php
@@ -1,3 +1,17 @@
 <link rel="stylesheet" href="css/main.css">
-<a class="main-btn" href="./?page=cadastro_cliente">Cliente</a>
-<a class="main-btn" href="./?page=cadastro_barbeiro">Barbeiro</a>
+<div class="container  cadastro-escolha">
+    <div class="row">
+        <div class="col-lg-6 col-md-12 imagem-escolha">
+            <a href="./?page=cadastro_barbeiro">
+                <h3>Venha ser nosso parceiro!</h3>
+                <img src="http://i.mlcdn.com.br/portaldalu/fotosconteudo/54754.jpg" class="img-fluid" alt="">
+            </a>
+        </div>
+        <div class="col-lg-6 col-md-12 imagem-escolha">
+            <a href="./?page=cadastro_cliente">
+                <h3>Agende seu corte agora!</h3>
+                <img src="https://encontresuafranquia.com.br/wp-content/uploads/2021/01/barber-shop-3173422_1920.jpg" class="img-fluid" alt="">
+            </a>
+        </div>
+    </div>
+</div>
--- inicio.php
+++ inicio.php
@@ -1,83 +1,201 @@
- <div class="container mt-5 mb-3">
-     <div class="row">
+<div class="container mt-5 mb-5">
+    <div class="row">
+        <div class="text-inicio">
+            <h1 class="mb-3">Mais avaliados</h1>
+            <h4 class="mb-3">Escolha o melhor horario para voçê, agende agora!</h4>
+        </div>
+        <div class="col-md-4 col-sm-12">
+            <a href="">
+                <!--COLOCAR O LINK DO PERFIL DO BARBERIO-->
+                <div class="card bg-dark text-white ">
+                    <div class="folder">
+                        <img src="https://www.emporioluz.com.br/wp-content/uploads/2021/07/CAPA.jpg" class="card-img" alt="...">
+                    </div>
+                    <div class="card-img-overlay">
+                        <div class="tudo">
+                            <div class="avatar">
+                                <img src="https://images.vexels.com/media/users/3/145922/raw/eb6591b54b2b6462b4c22ec1fc4c36ea-criador-de-avatar-feminino.jpg" class="img-fluid rounded-circle" alt="">
+                            </div>
+                            <div class="ms-2 c-details">
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                            </div>
+                        </div>
+                        <h4>RafaelArts</h4>
+                        <p>Aberto 8:00 / 19:00</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-md-4 col-sm-12">
+            <a href="">
+                <!--COLOCAR O LINK DO PERFIL DO BARBERIO-->
+                <div class="card bg-dark text-white ">
+                    <div class="folder">
+                        <img src="https://www.emporioluz.com.br/wp-content/uploads/2021/07/CAPA.jpg" class="card-img" alt="...">
+                    </div>
+                    <div class="card-img-overlay">
+                        <div class="tudo">
+                            <div class="avatar">
+                                <img src="https://images.vexels.com/media/users/3/145922/raw/eb6591b54b2b6462b4c22ec1fc4c36ea-criador-de-avatar-feminino.jpg" class="img-fluid rounded-circle" alt="">
+                            </div>
+                            <div class="ms-2 c-details">
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                            </div>
+                        </div>
+                        <h4>RafaelArts</h4>
+                        <p>Aberto 8:00 / 19:00</p>
+                    </div>
+                </div>
+            </a>
+        </div>
 
-         <div class="col-md-4 col-sm-12">
-             <div class="card p-3 mb-2">
-                 <div class="d-flex justify-content-between">
-                     <div class="d-flex flex-row align-items-center">
-                         <div class="icon"> <i class="bx bxl-mailchimp"></i> </div>
-                         <div class="ms-2 c-details">
-                         </div>
-                     </div>
-                 </div>
-                 <div class="mt-4">
-                     <p class="card-text placeholder-glow">
-                         <span class="placeholder col-7"></span>
-                         <span class="placeholder col-4"></span>
-                         <span class="placeholder col-4"></span>
-                         <span class="placeholder col-6"></span>
-                         <span class="placeholder col-8"></span>
-                     </p>
-                     <div class="mt-5">
-                         <a class="mt-3" href="#"> <span class="text1 login-text">Saiba mais</span></a>
-                     </div>
-                 </div>
-             </div>
-         </div>
+        <div class="col-md-4 col-sm-12">
+            <a href="">
+                <!--COLOCAR O LINK DO PERFIL DO BARBERIO-->
+                <div class="card bg-dark text-white ">
+                    <div class="folder">
+                        <img src="https://www.emporioluz.com.br/wp-content/uploads/2021/07/CAPA.jpg" class="card-img" alt="...">
+                    </div>
+                    <div class="card-img-overlay">
+                        <div class="tudo">
+                            <div class="avatar">
+                                <img src="https://images.vexels.com/media/users/3/145922/raw/eb6591b54b2b6462b4c22ec1fc4c36ea-criador-de-avatar-feminino.jpg" class="img-fluid rounded-circle" alt="">
+                            </div>
+                            <div class="ms-2 c-details">
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                            </div>
+                        </div>
+                        <h4>RafaelArts</h4>
+                        <p>Aberto 8:00 / 19:00</p>
+                    </div>
+                </div>
+            </a>
+        </div>
 
-         <div class="col-md-4 col-sm-12">
-             <div class="card p-3 mb-2">
-                 <div class="d-flex justify-content-between">
-                     <div class="d-flex flex-row align-items-center">
-                         <div class="icon"> <i class="bx bxl-dribbble"></i> </div>
-                         <div class="ms-2 c-details">
-                         </div>
-                     </div>
-                 </div>
-                 <div class="mt-4">
-                     <p class="heading login-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, laudantium incidunt. In veritatis amet,</p>
-                     <div class="mt-5">
-                         <a class="mt-3" href="#"> <span class="text1 login-text">Saiba mais</span> </a>
-                     </div>
-                 </div>
-             </div>
-         </div>
+    </div>
+</div>
+<div class="container-fluid mb-4 mt-5 divisao">
+    <div class="container p-5">
+        <div class="row ">
+            <div class="col-lg-6 col-md-12">
+                <div class="text-inicio">
+                    <h1>Todos tipos de cabelo!</h1>
+                    <h4>só criar seu cadastro para cortar com nossos melhores Cabeleireiros</h4>
+                    <div class="estilos-cabelo">
+                        <div class="container">
+                            <div class="row mb-5 mt-5">
+                                <div class="col-4">
+                                    <img src="https://mhmcdn.manualdohomemmoderno.com.br/?w=750&h=450&quality=31&clipping=landscape&url=https://manualdohomemmoderno.com.br/files/2021/06/cabelo-crespo.jpg" class="rounded float-start img-fluid" alt="...">
+                                </div>
+                                <div class="col-4">
+                                    <img src="https://www.sospedro.com.br/wp-content/uploads/2022/01/Design-sem-nome-2.png" class="rounded float-start img-fluid" alt="...">
+                                </div>
+                                <div class="col-4">
+                                    <img src="https://cortesdecabelo.pro.br/wp-content/uploads/2020/02/download-5.jpg" class="rounded float-start img-fluid" alt="...">
+                                </div>
+                            </div>
+                        </div>
 
-         <div class="col-md-4 col-sm-12 mb-4">
-             <div class="card p-3 mb-2">
-                 <div class="d-flex justify-content-between">
-                     <div class="d-flex flex-row align-items-center">
-                         <div class="icon"> <i class="bx bxl-reddit"></i> </div>
-                         <div class="ms-2 c-details">
-                         </div>
-                     </div>
-                 </div>
-                 <div class="mt-4">
-                     <p class="heading login-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, laudantium incidunt. In veritatis amet,</p>
-                     <div class="mt-5">
-                         <a class="mt-3" href="#"> <span class="text1 login-text">Saiba mais</span> </a>
-                     </div>
-                 </div>
-             </div>
-         </div>
+                        <div class="container">
+                            <div class="row mb-5 mt-5">
+                                <div class="col-4">
+                                    <img src="https://i.pinimg.com/736x/c1/8f/9d/c18f9d7c521e6b59e5689e8a24e5784a--mens-short-haircuts-mens-haircuts-for-thick-hair.jpg" class="rounded float-start img-fluid" alt="...">
+                                </div>
+                                <div class="col-4">
+                                    <img src="https://i.pinimg.com/736x/86/bb/3d/86bb3dcc13f1932b18ab00e1a9cfcd9f.jpg" class="rounded float-start img-fluid" alt="...">
+                                </div>
+                                <div class="col-4">
+                                    <img src="https://i.pinimg.com/originals/73/eb/1e/73eb1eb626ce2c70dea80320651d306f.jpg" class="rounded float-start img-fluid" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12">
+                <div class="col-12 text-center">
+                    <img src="./imagens/corte-homem.png" style="width: 70%; height:30%" class="card-img-top text-center" alt="...">
+                </div>
+                <div class="card-body p-4">
+                    <h4>Em duvida do corte?</h4>
+                    <p class="card-text">O Site de cabeleireiros líder no mundo, oferece serviços de beleza e cuidados com os cabelos. Oferecemos uma experiência que atende às necessidades de cabelos curto, longo, reto, encaracolado, ondulado a altamente texturizado – os profissionais da NextCut criarão um corte e estilo para se adequar ao seu visual e estilo de vida.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-     </div>
-     <hr>
-     <div class="row mt-5">
-         <div class="col-lg-6 col-md-12">
-             <img src="./imagens/corte-mulher.png" class="img-fluid" alt="corte de cabelo">
-         </div>
-         <div class="col-lg-6 col-md-12">
-             <img src="./imagens/corte-homem.png" class="img-fluid" alt="corte de cabelo">
-         </div>
-     </div>
 
- </div>
- <div class="container-fluid">
+<div class="container">
+    <div class="row">
+        <h1 class="text-center">Como funciona o app</h1>
+        <div class="container-video">
+            <div class="video1">
+                <video width="100%" height="100%" controls>
+                    <source src="movie.mp4" type="video/mp4">
+                </video>
+                <!--VICTOR FAZENDO VIDEOS DE COMO FUNCIONA -->
+            </div>
+            <div class="video2">
+                <video width="100%" height="100%" controls>
+                    <source src="movie.mp4" type="video/mp4">
+                </video>
+            </div>
+        </div>
+    </div>
+</div>
 
-     <img src="./imagens/divisao.png" style="width: 100%;" alt="">
+<div class="container-fluid mb-4 mt-5 divisao">
+    <div class="container p-5">
+        <div class="row ">
+            <div class="text-beneficio mb-4">
+                <h1>Benefícios dos Serviços da NextCut</h1>
+                <h4>A equipe da NextCut está focado na individualidade de cada um dos nossos clientes e estamos determinados a proporcionar a melhor experiência possível criando o ambiente personalizado e o visual ideal único para voçê.</h4>
+            </div>
 
- </div>
- <div class="container">
-    <div class="row"></div>
- </div>
+            <div class="col-md-4 col-sm-12 ">
+                <div class="card-beneficio text-center">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1584/1584892.png" class="card-img-top img-fluid" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Economize Tempo</h5>
+                        <p class="card-text">Valorizamos muito o tempo dos nossos clientes, através do agendamento pelo App podemos evitar filas e demoras para iniciar o serviço.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-12">
+                <div class="card-beneficio text-center">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2162/2162502.png" class="card-img-top img-fluid" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Estilistas Profissionais</h5>
+                        <p class="card-text">Existe um tipo de corte perfeito para cada indivíduo, podemos trabalhar juntos para formar um plano ideal para o seu cabelo que combine com seu modelo de rosto</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-12">
+                <div class="card-beneficio text-center">
+                    <img src="http://misterbarbershop.brindey.com.br/wp-content/uploads/2021/06/tesoura-e-pente-branco.png" class="card-img-top img-fluid" alt="...">
+
+                    <div class="card-body">
+                        <h5 class="card-title">Não sei o que por </h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>

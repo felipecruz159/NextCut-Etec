4ad8d3f0ee94f475520436a0b2efb7f478cac391
Rafa1922 <faelgomes2003@gmail.com>
2022-08-19 18:22:09 -0300

    allCard

--- header_logado.php
+++ header_logado.php
@@ -20,7 +20,7 @@
   <!-- /logo -->
   <div class="links-logado">
     <a href="">Cortes</a>
-    <a href="">Estetica</a>
+    <a href="">Estética</a>
     <a href="">Sombrancelhas</a>
   </div>
 
@@ -71,42 +71,42 @@
       </div>
       <div class="offcanvas-body">
         <div>
-          <div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
-            <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+          <div class="flex-shrink-0 p-3 bg-white" style="width: 300px;">
+            <a href="#" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
               <svg class="bi pe-none me-2" width="30" height="24">
                 <use xlink:href="#bootstrap" />
               </svg>
-              <span class="fs-5 fw-semibold">Collapsible</span>
+              <span class="fs-5 fw-semibold">Olá RF \Apelido...</span> <!--colocar o nome definido pelo usuario..-->
             </a>
             <ul class="list-unstyled ps-0">
               <li class="mb-1">
                 <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-                  Home
+                  Inicio
                 </button>
                 <div class="collapse show" id="home-collapse">
                   <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Overview</a></li>
-                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Updates</a></li>
-                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Reports</a></li>
+                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Cortes</a></li>
+                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Estética</a></li>
+                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Sombrancelhas</a></li>
                   </ul>
                 </div>
               </li>
               <li class="mb-1">
                 <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-                  Dashboard
+                  Categorias
                 </button>
                 <div class="collapse" id="dashboard-collapse">
                   <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Overview</a></li>
-                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Weekly</a></li>
-                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Monthly</a></li>
-                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Annually</a></li>
+                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Liso</a></li>
+                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Crespo</a></li>
+                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Cacheado</a></li>
+                    <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Ondulado</a></li>
                   </ul>
                 </div>
               </li>
               <li class="mb-1">
+                <!--
                 <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-                  Orders
                 </button>
                 <div class="collapse" id="orders-collapse">
                   <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
@@ -115,7 +115,7 @@
                     <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Shipped</a></li>
                     <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Returned</a></li>
                   </ul>
-                </div>
+                 </div> -->
               </li>
             </ul>
           </div>
@@ -126,9 +126,9 @@
             <strong>Conta</strong>
           </a>
           <ul class="dropdown-menu text-small shadow">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><a class="dropdown-item" href="#">Informações</a></li>
+            <li><a class="dropdown-item" href="#">Configurações</a></li>
+            <li><a class="dropdown-item" href="#">Privacidade Seguraança</a></li>
             <li>
               <hr class="dropdown-divider">
             </li>
@@ -139,103 +139,6 @@
 
     </div>
   </div>
-  <!-- <div id="headerRight" style="margin-right: 80px;"> 
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Perfil
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a class="dropdown-item" href="#">Perfil</a>
-          <a class="dropdown-item" href="#">Termos de uso</a>
-          <a class="dropdown-item" href="#">Configurações</a>
-          <a class="dropdown-item" href="./logout.php" style="color: red;">Sair / Início</a>
-        </div>
-      </div>
-    </div>
--->
 </header>
 
 
-
-
-<div style="display: none;">
-  <div class="header-logado" id="header">
-
-    <button class="btn btn-logado" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
-      <i class="bi bi-list"></i>
-    </button>
-
-    <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="staticBackdropLabel">Offcanvas</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <div class="flex-shrink-0 bg-white" style="width: 300px;">
-          <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
-            <svg class="bi pe-none me-2" width="30" height="24">
-              <use xlink:href="#bootstrap" />
-            </svg>
-            <span class="fs-5 fw-semibold">Olá, <?php echo @$nome ?></span> <!-- arrumar $sql -->
-          </a>
-          <ul class="list-unstyled ps-0">
-            <li class="mb-1">
-              <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-                Home
-              </button>
-              <div class="collapse show" id="home-collapse">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                  <li><a href="./?page=inicio" class="link-dark d-inline-flex text-decoration-none rounded">Inicio</a></li>
-                  <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Updates</a></li>
-                  <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Reports</a></li>
-                </ul>
-              </div>
-            </li>
-            <li class="mb-1">
-              <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-                Dashboard
-              </button>
-              <div class="collapse" id="dashboard-collapse">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                  <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Overview</a></li>
-                  <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Weekly</a></li>
-                  <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Monthly</a></li>
-                  <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Annually</a></li>
-                </ul>
-              </div>
-            </li>
-            <li class="mb-1">
-              <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-                Orders
-              </button>
-              <div class="collapse" id="orders-collapse">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                  <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">New</a></li>
-                  <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Processed</a></li>
-                  <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Shipped</a></li>
-                  <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Returned</a></li>
-                </ul>
-              </div>
-            </li>
-            <li class="border-top my-3"></li>
-            <li class="mb-1">
-              <div class="dropdown">
-                <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                  <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                  <strong>Conta</strong>
-                </a>
-                <ul class="dropdown-menu text-small shadow">
-                  <li><a class="dropdown-item" href="#">New project...</a></li>
-                  <li><a class="dropdown-item" href="#">Settings</a></li>
-                  <li><a class="dropdown-item" href="#">Profile</a></li>
-                  <li>
-                    <hr class="dropdown-divider">
-                  </li>
-                  <li><a class="dropdown-item" href="#">Sign out</a></li>
-                </ul>
-              </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
---
+++ todosCardBarbeiro.php
@@ -0,0 +1,166 @@
+<link rel="stylesheet" href="css/main.css">
+<style>
+    body {
+        margin: 0;
+        padding: 100px 0px;
+        box-sizing: border-box;
+    }
+</style>
+
+<div id="todos-cards">
+<div class="container">
+    <div class="mt-2"><h1>Todos</h1></div>
+    <hr>
+    <div class="row">
+
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <section>
+                <div class="card">
+                    <div class="card-content">
+
+                        <div class="image">
+                            <img src="https://home.odontosystem.com.br/wp-content/uploads/2018/08/riscos-clareamento-dental.jpg" alt="">
+                        </div>
+
+                        <div class="media-icons">
+                            <i class="bi bi-facebook"></i>
+                            <i class="bi bi-instagram"></i>
+                        </div>
+
+                        <div class="nome-fantasia">
+                            <span class="fantasia">Rafaelos'hair 2</span>
+                            <span class="nome">Rafael Gomes</span>
+                        </div>
+
+                        <div class="avaliacao">
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-half"></i>
+                            <i class="bi bi-star"></i>
+                        </div>
+
+                        <div class="button">
+                            <button class="perfil">Ver perfil</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <section>
+                <div class="card">
+                    <div class="card-content">
+
+                        <div class="image">
+                            <img src="https://img.freepik.com/fotos-gratis/emocoes-de-pessoas-lazer-do-estilo-de-vida-e-conceito-de-beleza-menina-asiatica-feliz-e-alegre-animada-dancando-e-se-divertindo-festejando-tocando-musica-ritmada-e-sorrindo-sobre-fundo-amarelo_1258-58916.jpg?w=2000" alt="">
+                        </div>
+
+                        <div class="media-icons">
+                            <i class="bi bi-facebook"></i>
+                            <i class="bi bi-instagram"></i>
+                        </div>
+
+                        <div class="nome-fantasia">
+                            <span class="fantasia">Rafaelos'hair 2</span>
+                            <span class="nome">Rafael Gomes</span>
+                        </div>
+
+                        <div class="avaliacao">
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-half"></i>
+                            <i class="bi bi-star"></i>
+                        </div>
+
+                        <div class="button">
+                            <button class="perfil">Ver perfil</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <section>
+                <div class="card">
+                    <div class="card-content">
+
+                        <div class="image">
+                            <img src="https://static.vecteezy.com/system/resources/previews/003/162/336/non_2x/close-up-view-senior-attractive-man-smiling-looking-free-photo.JPG" alt="">
+                        </div>
+
+                        <div class="media-icons">
+                            <i class="bi bi-facebook"></i>
+                            <i class="bi bi-instagram"></i>
+                        </div>
+
+                        <div class="nome-fantasia">
+                            <span class="fantasia">Rafaelos'hair 2</span>
+                            <span class="nome">Rafael Gomes</span>
+                        </div>
+
+                        <div class="avaliacao">
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-half"></i>
+                            <i class="bi bi-star"></i>
+                        </div>
+
+                        <div class="button">
+                            <button class="perfil">Ver perfil</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6 fundo-card">
+            <section>
+                <div class="card">
+                    <div class="card-content">
+
+                        <div class="image">
+                            <img src="http://www.scanct.com.br/estetialign/image/modelo-991.png" alt="">
+                        </div>
+
+                        <div class="media-icons">
+                            <i class="bi bi-facebook"></i>
+                            <i class="bi bi-instagram"></i>
+                        </div>
+
+                        <div class="nome-fantasia">
+                            <span class="fantasia">Rafaelos'hair 2</span>
+                            <span class="nome">Rafael Gomes</span>
+                        </div>
+
+                        <div class="avaliacao">
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-half"></i>
+                            <i class="bi bi-star"></i>
+                        </div>
+
+                        <div class="button">
+                            <button class="perfil">Ver perfil</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+    </div>
+</div>
+</div>
+
+
+
+
+
+
+
+<script src="/js/custom.js"></script>
